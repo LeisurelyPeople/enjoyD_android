3a64b6da8d31951e6a1c2580338d26505d3854b7
Wayne <rhkddn1657@gmail.com>
2020-07-06 20:13:53 +0900

    카카오 SDK 적용 작업

--- app/src/main/java/com/leisurely/people/enjoyd/ui/base/EnjoyDApplication.kt
+++ app/src/main/java/com/leisurely/people/enjoyd/ui/base/EnjoyDApplication.kt
@@ -1,9 +1,11 @@
 package com.leisurely.people.enjoyd.ui.base
 
 import android.app.Application
+import com.kakao.auth.KakaoSDK
 import com.leisurely.people.enjoyd.BuildConfig
 import com.leisurely.people.enjoyd.di.networkModule
 import com.leisurely.people.enjoyd.di.viewModelModule
+import com.leisurely.people.enjoyd.ui.sociallogin.KakaoSDKAdapter
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
@@ -21,6 +23,9 @@ class EnjoyDApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         instance = this
+
+        KakaoSDK.init(KakaoSDKAdapter(this))
+
         startKoin {
             if (BuildConfig.DEBUG) androidLogger()
             androidContext(this@EnjoyDApplication)
