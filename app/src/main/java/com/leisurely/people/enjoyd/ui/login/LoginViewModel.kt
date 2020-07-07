3a2bad37aa5bca1145085677052df8b50dab46c3
Wayne <rhkddn1657@gmail.com>
2020-07-07 14:06:57 +0900

    로그인 화면 디자인 작업  - 카카오 로그인 버튼 추가 및 listener 연결

--- app/src/main/java/com/leisurely/people/enjoyd/ui/login/LoginViewModel.kt
+++ app/src/main/java/com/leisurely/people/enjoyd/ui/login/LoginViewModel.kt
@@ -1,6 +1,10 @@
 package com.leisurely.people.enjoyd.ui.login
 
 import com.leisurely.people.enjoyd.ui.base.BaseViewModel
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.subjects.PublishSubject
+import io.reactivex.subjects.Subject
+import java.util.concurrent.TimeUnit
 
 /**
  * 로그인 액티비티의 ViewModel
@@ -8,5 +12,18 @@ import com.leisurely.people.enjoyd.ui.base.BaseViewModel
  * @author Wayne
  * @since v1.0.0 / 2020.07.06
  */
-class LoginViewModel: BaseViewModel() {
+class LoginViewModel : BaseViewModel() {
+
+    private val _kakaoLoginClick: PublishSubject<Unit> = PublishSubject.create()
+
+    val kakaoLoginClick: Subject<Unit> = _kakaoLoginClick
+
+    init {
+        _kakaoLoginClick
+            .throttleFirst(4000L, TimeUnit.MILLISECONDS, AndroidSchedulers.mainThread())
+            .subscribe {
+
+            }
+            .addDisposable()
+    }
 }
