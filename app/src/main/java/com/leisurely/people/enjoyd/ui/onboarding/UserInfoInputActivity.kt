c90edb903247142083ec94c66b94a80ca1fca1c0
Wayne <rhkddn1657@gmail.com>
2020-08-25 19:58:57 +0900

    회원가입 뷰 로직 작업  - 회원 가입 뷰 databinding, livedata 연결 작업  - DataExt, StringExt, EditTextExt 유틸 클래스 추가

--- app/src/main/java/com/leisurely/people/enjoyd/ui/onboarding/UserInfoInputActivity.kt
+++ app/src/main/java/com/leisurely/people/enjoyd/ui/onboarding/UserInfoInputActivity.kt
@@ -1,5 +1,6 @@
 package com.leisurely.people.enjoyd.ui.onboarding
 
+import android.app.DatePickerDialog
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
@@ -10,12 +11,16 @@ import android.text.style.ForegroundColorSpan
 import androidx.core.os.bundleOf
 import androidx.core.text.buildSpannedString
 import com.leisurely.people.enjoyd.R
+import androidx.lifecycle.Observer
 import com.leisurely.people.enjoyd.databinding.ActivityUserInfoInputBinding
 import com.leisurely.people.enjoyd.ui.base.BaseActivity
 import com.leisurely.people.enjoyd.ui.login.model.SocialLogin
 import com.leisurely.people.enjoyd.util.Constant
+import com.leisurely.people.enjoyd.util.ext.add
+import com.leisurely.people.enjoyd.util.ext.formatToViewDate
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import org.koin.core.parameter.parametersOf
+import java.util.*
 
 /**
  * 사용자 정보 입력 화면 (회원가입)
@@ -34,6 +39,14 @@ class UserInfoInputActivity :
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding.tvScreenTitle.text = getScreenTitleStringBuilder()
+
+        viewModel.startBirthDayPicker.observe(this, Observer {
+            openDatePicker()
+        })
+
+        viewModel.startBackScreen.observe(this, Observer {
+            finish()
+        })
     }
 
     private fun getScreenTitleStringBuilder(): SpannedString {
@@ -56,6 +69,26 @@ class UserInfoInputActivity :
                 )
             })
         }
+    }
+
+    private fun openDatePicker() {
+        val calendar = Calendar.getInstance()
+        DatePickerDialog(
+            this,
+            R.style.UserBirthDayDatePickerStyle,
+            DatePickerDialog.OnDateSetListener { _, year, month, dayOfMonth ->
+                val userBirthCalendar = Calendar.getInstance().apply {
+                    set(year, month, dayOfMonth)
+                }
+                viewModel.setUserBirthDayValue(userBirthCalendar.time.formatToViewDate())
+            },
+            calendar.get(Calendar.YEAR),
+            calendar.get(Calendar.MONTH),
+            calendar.get(Calendar.DAY_OF_MONTH)
+        ).apply {
+            datePicker.minDate = calendar.time.add(Calendar.YEAR, -3).time
+            datePicker.maxDate = calendar.time.add(Calendar.DAY_OF_MONTH, 280).time
+        }.show()
     }
 
     companion object {
--- app/src/main/java/com/leisurely/people/enjoyd/ui/onboarding/UserInfoInputViewModel.kt
+++ app/src/main/java/com/leisurely/people/enjoyd/ui/onboarding/UserInfoInputViewModel.kt
@@ -1,8 +1,13 @@
 package com.leisurely.people.enjoyd.ui.onboarding
 
-import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.leisurely.people.enjoyd.data.remote.data.request.SignUpRequest
+import com.leisurely.people.enjoyd.model.enums.Gender
 import com.leisurely.people.enjoyd.ui.base.BaseViewModel
 import com.leisurely.people.enjoyd.ui.login.model.SocialLogin
+import com.leisurely.people.enjoyd.util.ext.convertToServerDate
+import com.leisurely.people.enjoyd.util.lifecycle.LiveEvent
 
 /**
  * UserInfoInputActivity ViewModel
@@ -11,10 +16,146 @@ import com.leisurely.people.enjoyd.ui.login.model.SocialLogin
  * @since v1.0.0 / 2020.07.20
  */
 
-class UserInfoInputViewModel(private val socialLogin: SocialLogin) : BaseViewModel() {
+class UserInfoInputViewModel(socialLogin: SocialLogin) : BaseViewModel() {
 
-    init {
-        // TODO 회원가입 로직
-        Log.d("test1414", "성공")
+    /** 유저 소셜로그인 고유값 LiveData */
+    private val _userSocialId: MutableLiveData<Long> = MutableLiveData(socialLogin.socialId ?: -1)
+    val userSocialId: LiveData<Long> = _userSocialId
+
+    /** 유저 이름 LiveData */
+    private val _username: MutableLiveData<String> = MutableLiveData(socialLogin.name ?: "")
+    val username: MutableLiveData<String> = _username
+
+    /** 유저 이름 경고 문구 LiveData */
+    private val _showsUsernameWarningMsg: MutableLiveData<Boolean> =
+        MutableLiveData(_username.value.isNullOrEmpty())
+    val showsUsernameWarningMsg: LiveData<Boolean> = _showsUsernameWarningMsg
+
+    /** 유저 생일 입력 data picker 를 시작하기 위한 LiveData */
+    private val _startBirthDayPicker: LiveEvent<Unit> = LiveEvent()
+    val startBirthDayPicker: LiveEvent<Unit> = _startBirthDayPicker
+
+    /** 유저 생일 LiveData */
+    private val _userBirthDay: MutableLiveData<String> = MutableLiveData()
+    val userBirthDay: LiveData<String> = _userBirthDay
+
+    /** 유저 생일 경고 문구 LiveData */
+    private val _showsUserBirthDayWarningMsg: MutableLiveData<Boolean> = MutableLiveData(true)
+    val showsUserBirthDayWarningMsg: LiveData<Boolean> = _showsUserBirthDayWarningMsg
+
+    /** 유저 성별 LiveData */
+    private val _userGender: MutableLiveData<Int> = MutableLiveData<Int>().apply {
+        value = socialLogin.gender
     }
+    val userGender: LiveData<Int> = _userGender
+
+    /** 유저 성별 경고 문구 LiveData */
+    private val _showsUserGenderWarningMsg: MutableLiveData<Boolean> =
+        MutableLiveData(_userGender.value == null)
+    val showsUserGenderWarningMsg: MutableLiveData<Boolean> = _showsUserGenderWarningMsg
+
+    /** 유저 직업 LiveData */
+    private val _userJob: MutableLiveData<String> = MutableLiveData()
+    val userJob: LiveData<String> = _userJob
+
+    /** 유저 직업 경고 문구 LiveData */
+    private val _showsUserJobWarningMsg: MutableLiveData<Boolean> = MutableLiveData(true)
+    val showsUserJobWarningMsg: LiveData<Boolean> = _showsUserJobWarningMsg
+
+    /** 유저 상세 직업 LiveData */
+    private val _userDetailJob: MutableLiveData<String> = MutableLiveData()
+    val userDetailJob: MutableLiveData<String> = _userDetailJob
+
+    /** 유저 상세 직업 경고 문구 LiveData */
+    private val _showsUserDetailJobWarningMsg: MutableLiveData<Boolean> = MutableLiveData(true)
+    val showsUserDetailJobWarningMsg: LiveData<Boolean> = _showsUserDetailJobWarningMsg
+
+    /** 뒤로가기 버튼을 동작시키기 위한 LiveData */
+    private val _startBackScreen: LiveEvent<Unit> = LiveEvent()
+    val startBackScreen: LiveEvent<Unit> = _startBackScreen
+
+
+    /** 유저 이름 텍스트 변화 감지 메소드 */
+    fun onUsernameChanged(charSequence: CharSequence) {
+        _showsUsernameWarningMsg.value = charSequence.toString().isEmpty()
+    }
+
+    /** 액티비티 코드에 있는 데이터피커를 열기 위한 메소드  */
+    fun onClickUserBirthDay() {
+        _startBirthDayPicker.value = null
+    }
+
+    /** 액티비티 -> 뷰모델 값 셋팅을 위한 메소드 (유저 생일) */
+    fun setUserBirthDayValue(value: String?) {
+        value?.let {
+            _userBirthDay.value = value
+            _showsUserBirthDayWarningMsg.value = false
+        }
+    }
+
+    /** 유저 성별 RadioButton 클릭메소드 */
+    fun onUserGenderChanged(gender: Gender) {
+        _userGender.value = gender.value
+        _showsUserGenderWarningMsg.value = false // 유저 성별을 선택한 상태이므로 경고 메시지는 안보이도록 설정
+    }
+
+    /** 유저 직업 RadioButton 클릭메소드 */
+    fun onUserJobChanged(value: String, needsDetailJob: Boolean) {
+        _userJob.value = value
+        _showsUserJobWarningMsg.value = false
+        /** 사용자 직업이 "기타" 이면서 상세 직업칸이 비어있지 않을 경우 상세 직업 경고문 삭제 */
+        if (needsDetailJob) {
+            _showsUserDetailJobWarningMsg.value = _userDetailJob.value.isNullOrEmpty()
+        } else {
+            _showsUserDetailJobWarningMsg.value = false
+        }
+    }
+
+    /** 유저 상세 직업 텍스트 변화 감지 메소드 */
+    fun onUserDetailJobChanged(charSequence: CharSequence) {
+        _showsUserDetailJobWarningMsg.value = charSequence.toString().isEmpty()
+    }
+
+    /** 뒤로가기 버튼을 동작시키기 위한 메소드 */
+    fun onClickBackScreen() {
+        _startBackScreen.value = null
+    }
+
+    /** 회원가입 작업 메소드 */
+    fun onClickSignUp() {
+        val userSocialId = _userSocialId.value ?: kotlin.run {
+            /** 소셜아이디가 없는 경우 로그인 화면으로 다시 전환하기 */
+            _startBackScreen.value = null
+            return
+        }
+        val username = _username.value ?: kotlin.run {
+            showToast("이름을 입력해주세요.")
+            return
+        }
+        val userJob = _userJob.value?.let {
+            if (it == "기타") {
+                _userDetailJob.value ?: kotlin.run {
+                    showToast("상세 직업을 입력해주세요.")
+                    return
+                }
+            } else {
+                it
+            }
+        } ?: kotlin.run {
+            showToast("직업을 선택해주세요.")
+            return
+        }
+        val userBirthday = _userBirthDay.value?.convertToServerDate() ?: kotlin.run {
+            showToast("생일을 입력해주세요.")
+            return
+        }
+        val userGender = _userGender.value ?: kotlin.run {
+            showToast("성별을 선택해주세요.")
+            return
+        }
+        val test = SignUpRequest(userSocialId, username,userJob,userBirthday, userGender)
+
+        // TODO 회원가입 api 연결작업
+    }
+
 }
