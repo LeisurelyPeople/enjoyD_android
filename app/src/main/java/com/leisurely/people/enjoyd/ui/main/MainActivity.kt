b5dac9add32283e85dfe37490c45c970604d1e1d
Wayne <rhkddn1657@gmail.com>
2020-08-26 13:30:29 +0900

    메인 액티비티 getIntent 메소드에  clear top flag 추가

--- app/src/main/java/com/leisurely/people/enjoyd/ui/main/MainActivity.kt
+++ app/src/main/java/com/leisurely/people/enjoyd/ui/main/MainActivity.kt
@@ -19,7 +19,11 @@ class MainActivity : BaseActivity<ActivityMainBinding, MainViewModel>(R.layout.a
     companion object {
         fun getIntent(context: Context): Intent {
             return Intent(context, MainActivity::class.java).apply {
-                addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
+                addFlags(
+                    Intent.FLAG_ACTIVITY_CLEAR_TOP
+                            or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                            or Intent.FLAG_ACTIVITY_NEW_TASK
+                )
             }
         }
     }
