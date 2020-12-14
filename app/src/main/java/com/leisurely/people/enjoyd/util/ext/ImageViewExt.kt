ef6f4f6d768434ac99a2c422307ef69c56785c88
Wayne <rhkddn1657@gmail.com>
2020-12-14 12:16:23 +0900

    디자인 요청 사항 수정  - 마이페이지 탭 on 아이콘 추가  - 드라마 아이템 레이아웃 크기 및 내용 변경  - 배너 간격 및 정보 변경  - 시청중인 드라마 크기 및 내용 변경  - shrink 버그 수정 (cancel 될 경우 애니메이션이 멈추는 현상)

--- app/src/main/java/com/leisurely/people/enjoyd/util/ext/ImageViewExt.kt
+++ app/src/main/java/com/leisurely/people/enjoyd/util/ext/ImageViewExt.kt
@@ -4,6 +4,8 @@ import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.GenericTransitionOptions
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.bumptech.glide.request.RequestOptions
 import com.leisurely.people.enjoyd.R
 
 /**
@@ -14,13 +16,21 @@ import com.leisurely.people.enjoyd.R
  */
 
 /** Url imageView */
-@BindingAdapter("imgUrl")
-fun ImageView.loadImageUrl(url: String?) {
+@BindingAdapter(value = ["imgUrl", "imgRadius"], requireAll = false)
+fun ImageView.bindImageUrl(url: String?, radius: Float = 0F) {
     url?.let {
-        Glide.with(this)
-            .load(it)
-            .transition(GenericTransitionOptions.with(R.anim.fragment_fade_enter))
-            .into(this)
+        if (radius == 0F) {
+            Glide.with(this)
+                .load(it)
+                .transition(GenericTransitionOptions.with(R.anim.fragment_fade_enter))
+                .into(this)
+        } else {
+            Glide.with(this)
+                .load(it)
+                .apply(RequestOptions().transform(RoundedCorners(radius.toInt())))
+                .transition(GenericTransitionOptions.with(R.anim.fragment_fade_enter))
+                .into(this)
+        }
     }
 }
 
